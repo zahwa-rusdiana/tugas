10cd69917e080d2f763b5aca6497091dcc0daf82
zahwa-rusdiana <pppprusdiana@gmail.com>
2025-11-05 09:46:02 +0700

    length size

--- tugaspbadrus.cpp
+++ tugaspbadrus.cpp
@@ -5,7 +5,7 @@ int main() {
     string nama = "Putri Zahwa";
 
     cout << "String   : " << nama << endl;
-    cout << "Panjang  : " << nama.length() << endl;  
+    cout << "Panjang  : " << nama.length() << endl;   // atau nama.size()
 
     return 0;
 }
